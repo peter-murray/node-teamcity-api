592f218bd6fa6cd306df5ff04aa3120c339b837d
Austin Parker <aparker@apprenda.com>
2016-10-18 18:37:47 -0400

    add support for querying builds of build configs

---
+++ test/builds.test.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var expect = require("chai").expect
+  , TeamCityAPI = require("../lib/api")
+  , testData = require("./config/test-data")
+  , nockHelper = require("./nock-helper")
+  ;
+
+describe("Builds", function () {
+  var teamcity = new TeamCityAPI(testData);
+
+  before(function () {
+    nockHelper.record();
+  });
+
+  beforeEach(function () {
+    nockHelper.beforeEach(this);
+  });
+
+  afterEach(function () {
+    nockHelper.afterEach(this);
+  });
+
+  after(function () {
+    nockHelper.reset();
+  });
+
+  describe("#getBuilds()", function () {
+
+    it("should get builds by count for a named build config", function() {
+        var countObj = {count:10}
+        return teamcity.builds.getWithCount({name: "Test"},{dimensions: countObj})
+            .then(function (builds) {
+                expect(builds).to.exist;
+            });
+    });
+
+    it("should get builds by count for a build config by id", function() {
+        var countObj = {count:10}
+        return teamcity.builds.getWithCount({id: "Test_Config"}, {dimensions: countObj})
+            .then(function (builds) {
+                expect(builds).to.exist;
+            });
+    });
+
+    it("should get build by its id", function() {
+        return teamcity.builds.get({id: 1001})
+            .then(function (builds) {
+                expect(builds).to.exist;
+            });
+    });
+
+    });
+});
